2a097409a34c35847dfc2d2a4a5e353d19ed8adb
astonishiing <wcardoso4510@gmail.com>
2025-02-27 15:33:47 -0300

    Criacao da classe main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+                Quadrado quadrado = new Quadrado();
+                Triangulo triangulo = new Triangulo("Azul", 5, 12);
+                Circulo circulo = new Circulo("Verde", 10.2);
+                Retangulo retangulo = new Retangulo("Rosa", 32, 12);
 
-    }
-}
+                quadrado.setCor("Roxo");
+                quadrado.setLado1(4);
+
+                System.out.println(quadrado);
+                System.out.println(triangulo);
+                System.out.println(circulo);
+                System.out.println(retangulo);
+            }
+        }
+
