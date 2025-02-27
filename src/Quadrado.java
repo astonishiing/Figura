f3d26cbf38bbe3a3d784e8eb0257a8cc6a350196
astonishiing <wcardoso4510@gmail.com>
2025-02-27 15:05:00 -0300

    Criacao da figura <Quadrado>

--- src/Quadrado.java
+++ src/Quadrado.java
@@ -2,6 +2,24 @@ public class Quadrado extends Retangulo{
     //Não há adicao de atributos
 
     //Construtores
+    public Quadrado(){
 
+    }
+    public Quadrado(String cor, double lado1, double lado2){
+        super(cor, lado1, lado2);
+    }
+    public Quadrado(String cor, double lado){
+        lado = super.getLado1();
+    }
+
+    @Override
+    public double area(){
+        return this.getLado1() * this.getLado1();
+    }
+
+    @Override
+    public String toString(){
+        return "Quadrado [area()" + area() + ", cor" + super.getCor();
+    }
 
 }
