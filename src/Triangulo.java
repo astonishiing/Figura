2a410222088ed3839f0bb267d9ebeda1a3b5c348
astonishiing <wcardoso4510@gmail.com>
2025-02-27 14:55:51 -0300

    adção dos métodos getters e setters

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -1,5 +1,34 @@
-public class Triangulo {
+public class Triangulo extends Figura {
+
+    //Atributos
     double base;
     double altura;
+
+    //Métodos construtores
+    public Triangulo(){
+
+    }
+    public Triangulo (String cor, double base, double altura){
+        super(cor);
+        this.base = base;
+        this.altura = altura;
+    }
+
+    //Métodos especiais
+    public double getAltura(){
+        return altura;
+    }
+    public void setAltura(double altura){
+        this.altura = altura;
+    }
+
+    public double getBase(){
+        return base;
+    }
+    public void setBase(double base){
+        this.base = base;
+    }
+
+
 
 }
